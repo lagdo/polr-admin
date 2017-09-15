e6d8927eb1d8332ae18295b03e85084303a8d276
feuzeu <thierry.feuzeu@gmail.com>
2017-09-14 21:36:29 -0400

    Changed the route middlewares.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,8 +26,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'user.check' => \App\Http\Middleware\UserCheck::class,
+        'user.guest' => \App\Http\Middleware\UserGuest::class,
     ];
 }
