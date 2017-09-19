b947b7523b8721dc7f61a23d6185cfaa3f974240
feuzeu <thierry.feuzeu@gmail.com>
2017-09-18 21:33:27 -0400

    Implemented the login and password reset features.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,18 +15,29 @@
 Route::group(array('middleware' => 'user.guest'), function()
 {
     Route::get('/', array('as' => 'index', 'uses' => 'HomeController@index'));
-    Route::get('logout', array('as' => 'logout', 'uses' => 'UserController@logout'));
+    Route::get('/logout', array('as' => 'logout', 'uses' => 'UserController@logout'));
 });
 
 Route::group(array('middleware' => 'user.check'), function()
 {
-    Route::get('login', array('as' => 'showLogin', 'uses' => 'UserController@showLogin'));
-    // Route::get('signup', array('as' => 'showSignup', 'uses' => 'UserController@showSignup'));
-    // Route::get('forgot', array('as' => 'showForgot', 'uses' => 'UserController@showForgot'));
+    Route::get('/login', array('as' => 'showLogin', 'uses' => 'UserController@showLogin'));
+    if(true /*env('POLR_ALLOW_ACCT_CREATION')*/)
+    {
+        Route::get('/signup', array('as' => 'showSignup', 'uses' => 'UserController@showSignup'));
+    }
+    Route::get('/activate/{username}/{recoveryKey}', ['as' => 'activate', 'uses' => 'UserController@activate']);
+    Route::get('/password/lost', array('as' => 'showLostPassword', 'uses' => 'UserController@showLostPassword'));
+    Route::get('/password/reset/{username}/{recoveryKey}',
+        ['as' => 'getResetPassword', 'uses' => 'UserController@resetPassword']);
 
     Route::post('login', array('as' => 'postLogin', 'uses' => 'UserController@postLogin'));
-    // Route::post('signup', array('as' => 'postSignup', 'uses' => 'UserController@postSignup'));
-    // Route::post('forgot', array('as' => 'postForgot', 'uses' => 'UserController@postForgot'));
+    if(true /*env('POLR_ALLOW_ACCT_CREATION')*/)
+    {
+        Route::post('/signup', array('as' => 'postSignup', 'uses' => 'UserController@postSignup'));
+    }
+    Route::post('/password/lost', array('as' => 'postLostPassword', 'uses' => 'UserController@postLostPassword'));
+    Route::post('/password/reset/{username}/{recoveryKey}',
+        ['as' => 'postResetPassword', 'uses' => 'UserController@resetPassword']);
 });
 
 // Jaxon request processor
