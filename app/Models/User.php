2a487c435f21ad80da767c41fa8fb5695db0fbaf
feuzeu <thierry.feuzeu@gmail.com>
2017-09-18 21:26:51 -0400

    The User model ignores the remember_token field. - This field is not in the database users table.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,30 @@
 <?php
 namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
+class User extends Model implements AuthenticatableContract, AuthorizableContract, CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword;
 
-class User extends Model {
     protected $table = 'users';
+
+    /**
+     * Overrides the method to ignore the remember token.
+     */
+    public function setAttribute($key, $value)
+    {
+        // $isRememberTokenAttribute = ($key == $this->getRememberTokenName());
+        // if (!$isRememberTokenAttribute)
+        if($key != $this->getRememberTokenName())
+        {
+            parent::setAttribute($key, $value);
+        }
+    }
 }
