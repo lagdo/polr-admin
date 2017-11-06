7283622e0ab2a63b14a3c1b5793f74fb011d5061
feuzeu <thierry.feuzeu@gmail.com>
2017-11-05 19:58:00 -0500

    Javascript and CSS files are now loaded from CDN, when available.

--- resources/views/css.blade.php
+++ resources/views/css.blade.php
@@ -1,11 +1,12 @@
+    <!-- Datatables from https://datatables.net/download/#bs/dt-1.10.12/r-2.1.0 -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/v/bs/dt-1.10.12/r-2.1.0/datatables.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jvectormap/2.0.4/jquery-jvectormap.min.css" />
     <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/base.css" />
-    <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/font-awesome.min.css" />
     <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/admin.css" />
     <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/index.css" />
     <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/stats.css" />
-    <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/datatables.min.css" />
-    <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/jquery-jvectormap.css" />
-    <link rel="stylesheet" href="{{ url('lagdo/polr/admin') }}/css/bootstrap-datetimepicker.min.css" />
     <style>
     #mapChart {
         height: 24em;
--- resources/views/js.blade.php
+++ resources/views/js.blade.php
@@ -1,11 +1,11 @@
-<script src="{{ url('lagdo/polr/admin') }}/js/datatables.min.js"></script>
-<script src="{{ url('lagdo/polr/admin') }}/js/lodash.min.js"></script>
-<script src="{{ url('lagdo/polr/admin') }}/js/chart.bundle.min.js"></script>
-<script src="{{ url('lagdo/polr/admin') }}/js/datatables.min.js"></script>
+<!-- Datatables from https://datatables.net/download/#bs/dt-1.10.12/r-2.1.0 -->
+<script src="https://cdn.datatables.net/v/bs/dt-1.10.12/r-2.1.0/datatables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.16.4/lodash.min.js"></script>
 <script src="{{ url('lagdo/polr/admin') }}/js/jquery-jvectormap.min.js"></script>
 <script src="{{ url('lagdo/polr/admin') }}/js/jquery-jvectormap-world-mill.js"></script>
-<script src="{{ url('lagdo/polr/admin') }}/js/moment.min.js"></script>
-<script src="{{ url('lagdo/polr/admin') }}/js/bootstrap-datetimepicker.min.js"></script>
 <script src="{{ url('lagdo/polr/admin') }}/js/base.js"></script>
 <script src="{{ url('lagdo/polr/admin') }}/js/home.js"></script>
 <script src="{{ url('lagdo/polr/admin') }}/js/stats.js"></script>
